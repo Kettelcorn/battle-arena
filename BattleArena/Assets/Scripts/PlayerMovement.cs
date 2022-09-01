e06a014af0e67f50afb58d4684cbac615b3d09f1
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2022-09-01 08:02:30 -0700

    Smooth transitions between running, jumping, and landing

--- BattleArena/Assets/Scripts/PlayerMovement.cs
+++ BattleArena/Assets/Scripts/PlayerMovement.cs
@@ -14,10 +14,6 @@ public class PlayerMovement : MonoBehaviour
     private int jumpTracker;
     private Animator anim;
 
-    private bool isJumping;
-    private bool isFalling;
-    private bool isGrounded;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -41,37 +37,41 @@ public class PlayerMovement : MonoBehaviour
         // allows player to jump and double jump
         if (Input.GetButtonDown("Jump") && jumpTracker < 2)
         {
-            isJumping = true;
-            isGrounded = false;
             rb.velocity = new Vector3(0, jumpSpeed, 0);
             Jump();
             jumpTracker++;
         }
 
-        if (rb.velocity.y < 0) isFalling = true;
-        else isFalling = false;
-
-        if (x != 0 || z != 0)
-            Running(x, z);
+        if (x != 0 || z != 0 && anim.GetBool("isGrounded"))
+            Run(x, z);
 
     }
 
-    private void Running(float x, float z)
+    private void Run(float x, float z)
     {
+        anim.SetBool("isRunning", true);
         anim.SetFloat("Blend", Math.Abs(x) + Math.Abs(z));
     }
 
     private void Jump()
     {
-        anim.SetTrigger("Jump");
+        anim.SetTrigger("isJumping");
+        anim.SetBool("isGrounded", false);
+    }
+
+    private void Land()
+    {
+        anim.SetBool("isGrounded", true);
     }
 
 
     // resets jump tracker upon collision with ground
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) jumpTracker = 0;
-        isGrounded = true;
-        isJumping = false;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpTracker = 0;
+            Land();
+        }
     }
 }
